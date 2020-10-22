c9b4f0c4cdda68bc228aff5e3929db84f589f8db
QiZhuhua <an1ju@163.com>
2020-10-22 10:21:09 +0800

    添加项目文件。

---
+++ Qi.NetFly.Core/Model/Author.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Qi.NetFly.Core.Model
+{
+    /// <summary>
+    /// 自我介绍
+    /// </summary>
+    public class Author
+    {
+        public string Name { get; } = "qizhuhua";
+        public string Email { get; } = "an1ju@163.com";
+        public string Phone { get; } = "+8613940282230";
+        public string Location { get; } = "Shenyang,Liaoning,China";
+    }
+}
---
+++ Qi.NetFly/Controllers/AboutController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Qi.NetFly.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AboutController : ControllerBase
+    {
+        // GET: api/<AboutController>
+        [HttpGet]
+        public Qi.NetFly.Core.Model.Author Get()
+        {
+            return new Qi.NetFly.Core.Model.Author();
+        }
+
+        
+    }
+}
